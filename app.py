7dbd9e703d1af5ab436977d90cbc207eb146a372
Apurva-bhoir <apurvabhoir1711@gmail.com>
2025-04-17 16:40:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,135 @@
+
+import pickle
+import streamlit as st
+import requests
+import pandas as pd
+
+
+# Function to fetch the poster from TMDb API
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    response = requests.get(url)
+    data = response.json()
+    poster_path = data.get('poster_path')
+
+    if poster_path:
+        full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    else:
+        full_path = "https://via.placeholder.com/500x750?text=No+Poster"
+
+    return full_path
+
+
+# Function to recommend movies based on similarity
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+
+    recommended_movie_names = []
+    recommended_movie_posters = []
+
+    for i in distances[1:6]:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+        recommended_movie_posters.append(fetch_poster(movie_id))
+
+    return recommended_movie_names, recommended_movie_posters
+
+
+# --- Load data ---
+movies = pickle.load(open('movie_dict.pkl', 'rb'))  # Assuming it's a dict
+movies = pd.DataFrame(movies)  # Convert to DataFrame if needed
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+# --- Streamlit App ---
+st.header('🎬 Movie Recommender System')
+
+# Dropdown for selecting movie
+movie_list = movies['title'].values
+selected_movie = st.selectbox("Type or select a movie from the dropdown", movie_list)
+
+# Show recommendations
+if st.button('Show Recommendation'):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+
+    # Display recommendations in 5 columns
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
+
+# import streamlit as st
+# from click import option
+# import pickle
+# import pandas as pd
+# import requests
+#
+# # to fetch poster
+# def fetch_poster(movie_id):
+#     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(
+#         movie_id)
+#     data = requests.get(url)
+#     data = data.json()
+#     poster_path = data['poster_path']
+#     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+#     return full_path
+#
+# # function for recommend button
+# def recommend(movie):
+#     movie_index = movies[movies['title'] == movie].index[0]
+#     distances = similarity[movie_index]
+#     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+#
+#     recommended_movies = []
+#     recommended_movies_posters = []
+#     for i in movies_list:
+#         movie_id = movies.iloc[i[0]].movie_id
+#
+#         recommended_movies.append(movies.iloc[i[0]]['title'])
+#         # fetch poster from API
+#         recommended_movies_posters.append(fetch_poster(movie_id))
+#     return recommended_movies,recommended_movies_posters
+#
+# movies_dict = pickle.load(open('movie_dict.pkl','rb'))
+# movies = pd.DataFrame(movies_dict)
+#
+# similarity = pickle.load(open('similarity.pkl','rb'))
+#
+# st.title('Movie Recommender System')
+#
+# selected_movie_name = st.selectbox('Hello?', movies['title'].values)
+#
+# if st.button('Recommend'):
+#     names,posters = recommend(selected_movie_name)
+#     # for i in recommendations:
+#     #     st.write(i)
+#
+#     col1,col2,col3,col4,col5 = st.beta_columns(5)
+#     with col1:
+#         st.text(names[0])
+#         st.image(posters[0])
+#     with col2:
+#         st.text(names[1])
+#         st.image(posters[1])
+#     with col3:
+#         st.text(names[2])
+#         st.image(posters[2])
+#     with col4:
+#         st.text(names[3])
+#         st.image(posters[3])
+#     with col5:
+#         st.text(names[4])
+#         st.image(posters[4])
